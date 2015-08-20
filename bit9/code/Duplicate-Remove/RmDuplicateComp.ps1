5e1a696001893701e16e04a9f6e0a1e73d1a6ff1
sensitiveTooth <dxhepa@gmail.com>
2015-08-20 13:49:46 -0400

    Added comments

--- bit9/code/Duplicate-Remove/RmDuplicateComp.ps1
+++ bit9/code/Duplicate-Remove/RmDuplicateComp.ps1
@@ -1,15 +1,33 @@
+#####################
+# Set up API
+#####################
 $apiKey = "Your API Key Here"
 $URLResource = "https://serverURL/api/bit9platform/v1/computer"
 $ActiveComputers = $URLResource+"?q=deleted:false"
 
-$Computers = Invoke-RestMethod -Uri $ActiveComputers -Method Get -Header @{ "X-Auth-Token" = $apiKey } 
+#####################
+# API Call
+#####################
+$Computers = Invoke-RestMethod -Uri $ActiveComputers -Method Get -Header @{ "X-Auth-Token" = $apiKey }
 
+#####################
+# Import all the data into a hash table.
+#####################
 $HashTable = @{}
 foreach($Computer in $Computers)
 {
 	$HashTable[$Computer.name] = $Computer.id
 }
+
+#####################
+# GetEnumerator Function will maintain unique values based on the sort given. Supports multi-sort
+#####################
 $HashTable.GetEnumerator() | sort Name,Value -Descending
+
+#####################
+# If the machine is not in the hash table, it is a duplicate
+# Check each active machine against hash table, and if it doesn't exist in the hash table, delete it from the server.
+#####################
 foreach ($Instance in $Computers)
 {
     $comp = $Instance.name
